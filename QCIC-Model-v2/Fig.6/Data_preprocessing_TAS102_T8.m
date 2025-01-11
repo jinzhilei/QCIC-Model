ac097b26562214a2b673eb74583e8590917ac685
jinzhilei <142483163+jinzhilei@users.noreply.github.com>
2025-01-12 06:42:38 +0800

    Add files via upload

---
+++ QCIC-Model-v2/Fig.6/Data_preprocessing_TAS102_T8.m
@@ -0,0 +1,40 @@
+function Data_preprocessing_TAS102_T8()
+
+sample = 4000; 
+Non_PP = zeros(sample,1);
+PP = zeros(sample,1);
+
+global par
+par.V_tumor = 2.572e-9;
+par.V_C = 0.8;
+par.V_E = 0.37;
+par.T = 4e8;
+
+for i=1:sample
+
+    file=['./TAS102_VP/','VP',num2str(i),'.mat'];
+    A=cell2mat(struct2cell(load(file)));
+    [~,~,T8_C,X1_C,X2_C,X3_C]=cell_data(A);
+
+    T = X1_C(1:56:end)+X2_C(1:56:end)+X3_C(1:56:end);
+    V_before = ( par.V_tumor * par.T * par.V_C ) / par.V_E;
+    V_after  = ( par.V_tumor .* T .* par.V_C ) / par.V_E;
+    V = V_after / V_before - 1;
+
+    if V(2)>=0.2
+        PP(i) = T8_C(57,1);
+    else
+        Non_PP(i) = T8_C(57,1);
+    end
+
+end
+
+Non_PP(Non_PP==0)=[];
+PP(PP==0)=[];
+
+file='./Data_ClinProAnalysis/T8_NP_TAS102.mat';
+save(file,'Non_PP');
+file='./Data_ClinProAnalysis/T8_P_TAS102.mat';
+save(file,'PP');
+
+end
---
+++ QCIC-Model-v2/Fig.6/Data_preprocessing_TASBEV_T1.m
@@ -0,0 +1,40 @@
+function Data_preprocessing_TASBEV_T1()
+
+sample = 4000; 
+Non_PP = zeros(sample,1);
+PP = zeros(sample,1);
+
+global par
+par.V_tumor = 2.572e-9;
+par.V_C = 0.8;
+par.V_E = 0.37;
+par.T = 4e8;
+
+for i=1:sample
+
+    file=['./TASBEV_VP/','VP',num2str(i),'.mat'];
+    A=cell2mat(struct2cell(load(file)));
+    [T1_C,~,~,X1_C,X2_C,X3_C]=cell_data(A);
+
+    T = X1_C(1:56:end)+X2_C(1:56:end)+X3_C(1:56:end);
+    V_before = ( par.V_tumor * par.T * par.V_C ) / par.V_E;
+    V_after  = ( par.V_tumor .* T .* par.V_C ) / par.V_E;
+    V = V_after / V_before - 1;
+
+    if V(2)>=0.2
+        PP(i) = T1_C(57,1);
+    else
+        Non_PP(i) = T1_C(57,1);
+    end
+
+end
+
+Non_PP(Non_PP==0)=[];
+PP(PP==0)=[];
+
+file='./Data_ClinProAnalysis/Th1_NP_TASBEV.mat';
+save(file,'Non_PP');
+file='./Data_ClinProAnalysis/Th1_P_TASBEV.mat';
+save(file,'PP');
+
+end
