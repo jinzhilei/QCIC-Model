9cbaaffcce754534f063f7dff1ea3c53f39e7820
jinzhilei <142483163+jinzhilei@users.noreply.github.com>
2025-01-12 21:49:39 +0800

    Add files via upload

---
+++ QCIC-Model-V2/Fig.7/Fig.7b and 7d/pic_TAS102_ROS.m
@@ -0,0 +1,57 @@
+function pic_TAS102_ROS()
+
+file1='./TSA102_ROC/T_C_sample.mat';
+A=cell2mat(struct2cell(load(file1)));
+
+file2='./TSA102_ROC/T1_C_sample.mat';
+T1=cell2mat(struct2cell(load(file2)));
+
+file3='./TSA102_ROC/T8_C_sample.mat';
+T8=cell2mat(struct2cell(load(file3)));
+
+file4='./TSA102_ROC/T1Treg_sample.mat';
+T1Treg=cell2mat(struct2cell(load(file4)));
+
+file5='./TSA102_ROC/T8Treg_sample.mat';
+T8Treg=cell2mat(struct2cell(load(file5)));
+
+x=0:0.1:1;
+y=x;
+plot(x,y,'--','color','#808080','LineWidth',2);
+hold on
+
+[X_T8,Y_T8,~,AUC_T8] = perfcurve(A,T8,1);
+P_T8 = plot(X_T8,Y_T8,'-','color','#A93CC4','linewidth',2);
+hold on
+
+[X_T1,Y_T1,~,AUC_T1] = perfcurve(A,T1,1);
+P_T1 = plot(X_T1,Y_T1,'-','color','#FF0000','linewidth',2);
+hold on
+
+[X_T1Treg,Y_T1Treg,~,AUC_T1Treg] = perfcurve(A,T1Treg,1);
+P_T1Treg = plot(X_T1Treg,Y_T1Treg,'-','color','#0968F7','linewidth',2);
+hold on
+
+[X_T8Treg,Y_T8Treg,~,AUC_T8Treg] = perfcurve(A,T8Treg,1);
+P_T8Treg = plot(X_T8Treg,Y_T8Treg,'-','color','#33CC52','linewidth',2);
+hold on
+
+AUC=[AUC_T8,AUC_T1,AUC_T1Treg,AUC_T8Treg]
+
+
+%% Output image setting
+
+set(gca,'XLim',[0 1]);
+set(gca,'YLim',[0 1]);
+set(gca,'Linewidth',1);
+set(gca,'FontSize',22);
+set(gca,'FontName','Arial');
+xlabel('1-Specificity');
+ylabel('Sensitivity');
+title('TAS-102')
+legend([P_T8 P_T1 P_T1Treg P_T8Treg],'CTL           AUC=0.77','Th1            AUC=0.75','Th1/Treg    AUC=0.72','CTL/Treg   AUC=0.62','location','southeast');
+legend('boxon')
+set(gcf,'unit','centimeters','position',[3 15 18 16])
+print('ROC_TAS-102','-dpng','-r600')
+
+end
